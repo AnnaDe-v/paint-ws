8ec756511456fd28886b45d8a0f29cf9a47e6505
AnnaDe-v <annade.v88@gmail.com>
2022-07-23 09:19:16 +0300

    fix draw params

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -3,13 +3,15 @@ import canvasState from '../store/canvasState';
 import toolState from '../store/toolState';
 import '../styles/toolbar.scss'
 import Brush from '../tools/Brush';
+import Eraser from '../tools/Eraser';
+import Rectangle from '../tools/Rectangle';
 
 const Toolbar = () => {
     return (
         <div className='toolbar'>
             <button className='toolbar__button brush' onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></button>
-            <button className='toolbar__button rectangle'></button>
-            <button className='toolbar__button eraser'></button>
+            <button className='toolbar__button rectangle' onClick={() => toolState.setTool(new Rectangle(canvasState.canvas))}></button>
+            <button className='toolbar__button eraser' onClick={() => toolState.setTool(new Eraser(canvasState.canvas))}></button>
         </div>
     );
 };
--- src/tools/Rectangle.ts
+++ src/tools/Rectangle.ts
@@ -17,15 +17,34 @@ export default class Brush extends Tool {
         this.mouseDown = false
     }
     mouseDown(e) {
-
+        this.mouseDown = true
+        this.ctx.beginPath()
+        this.startX = e.pageX - e.target.offsetLeft;
+        this.startY = e.pageY - e.target.offsetTop;
+        this.saved = this.canvas.toDataURL()
     }
 
     mouseMove(e: any): any {
-
+        if (this.mouseDown) {
+            let currentX = e.pageX - e.target.offsetLeft;
+            let currentY = e.pageY - e.target.offsetTop;
+            this.width = currentX - this.startX;
+            this.height = currentY - this.startY;
+            this.draw(this.startX, this.startY, this.width, this.height)
+        }
     }
 
-    draw(x: number, y: number, h: number, w: number) {
-
+    draw(x: number, y: number, w: number, h: number) {
+        const img = new Image()
+        img.src = this.saved
+        img.onload = () => {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+            this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height)
+            this.ctx.beginPath()
+            this.ctx.rect(x, y, w, h)
+            this.ctx.fill()
+            this.ctx.stroke()
+        }
     }
 
 
